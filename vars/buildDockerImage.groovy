6bf4cda2769359cdd4e3181c2d9615732666d597
Maksym Prokopov <mprokopov@gmail.com>
2019-08-08 13:11:43 +0200

    added buildDockerImage

---
+++ vars/buildDockerImage.groovy
@@ -0,0 +1,16 @@
+/*
+ call docker build in dind agent
+ */
+def call(Map parameters = [:] {
+    def image = parameters.image
+    def tag = parameters.tag
+    def tagged_image = image + ":" + tag
+
+    sh """
+          \$(aws ecr get-login --no-include-email --region eu-central-1)
+          docker build . -t ${tagged_image}
+          docker push ${tagged_image}
+       """
+
+  return "${IMAGE}:${BUILD_ID}"
+}
